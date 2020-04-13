62306301eff9f3e7fcb87069c0b774e78375d17e
benoitantelme <benoitantelme@gmail.com>
2020-04-13 16:33:20 +0100

    Started the dataframe cleaning for PM10 data and some refactoring

--- airpollpy/src/csv_dataset.py
+++ airpollpy/src/csv_dataset.py
@@ -1,7 +1,20 @@
 import pandas as pd
+from pandas import DataFrame
 
 
-def get_dataset(path: str):
+def get_dataframe(path: str) -> DataFrame:
     return pd.read_csv(path)
+
+
+def clean_pm10_timeseries(path: str) -> DataFrame:
+    df = pd.read_csv(path)
+
+    df.drop(['Namespace', 'AirQualityNetwork', 'AirQualityStationEoICode', 'SamplingPoint', 'SamplingProcess', 'AirPollutantCode',
+             'DatetimeEnd', 'Verification'], axis=1, inplace=True)
+
+    # clean invalid rows
+    df = df[df['Validity'] != -1]
+
+    return df
 
 
--- airpollpy/tests/src/test_csv_dataset.py
+++ airpollpy/tests/src/test_csv_dataset.py
@@ -5,7 +5,17 @@ def test_test():
     assert True
 
 
-def test_get_dataset():
-    ds = csv_dataset.get_dataset("../../data/test/csv_test_1.csv")
-    assert len(ds) == 3
-    assert len(ds.columns) == 3
+def test_get_dataframe():
+    df = csv_dataset.get_dataframe("../../data/test/csv_test_1.csv")
+    assert len(df) == 3
+    assert len(df.columns) == 3
+
+
+def test_clean_pm10_timeseries():
+    path = "../../data/main/original/PM10/London/GB_5_22642_2020_timeseries.csv"
+    ds = csv_dataset.get_dataframe(path)
+    assert len(ds.columns) == 17
+
+    ds = csv_dataset.clean_pm10_timeseries(path)
+    assert len(ds.columns) == 9
+    assert True
