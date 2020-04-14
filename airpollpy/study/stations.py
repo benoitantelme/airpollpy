aa468dcb1b01209d31c89a4aa5b9a57fd0be7b5f
benoitantelme <benoitantelme@gmail.com>
2020-04-14 11:59:14 +0100

    Code to filter worst traffic urban stations for each city

--- airpollpy/study/stations.py
+++ airpollpy/study/stations.py
@@ -8,4 +8,20 @@ def get_stations_data(path: str) -> DataFrame:
     return df
 
 
+def get_worst_stations(path: str) -> DataFrame:
+    """
+    :param path: path of the data file
+    :return: the data set limited to the worst traffic urban station per city
+    """
+
+    df = get_stations_data(path)
+    df = df[df['type_of_station'] == 'Traffic']
+    df = df[df['station_type_of_area'] == 'urban']
+
+    # keep only worse values for criteria
+    df = df.loc[df.groupby(["city_name"])["statistic_value (µg/m3)"].idxmax()]
+
+    return df
+
+
 
--- airpollpy/tests/study/test_stations.py
+++ airpollpy/tests/study/test_stations.py
@@ -1,7 +1,22 @@
 from study.stations import get_stations_data
+from study.stations import get_worst_stations
+
+path = "../../data/main/cleaned/others/2013_pm10_monitoring_stations.csv"
 
 
 def test_get_stations_data():
-    path = "../../data/main/cleaned/others/2013_pm10_monitoring_stations.csv"
     df = get_stations_data(path)
     assert df is not None
+
+
+def test_get_worst_stations():
+    df = get_stations_data(path)
+    size = len(df)
+
+    df = get_worst_stations(path)
+    assert df is not None
+    assert len(df) < size
+    # number of ''traffic urban'' cities
+    assert len(df) == 317
+
+
