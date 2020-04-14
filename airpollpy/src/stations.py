274277aba47bf780fbce26fa29d9b87a26d93de1
benoitantelme <benoitantelme@gmail.com>
2020-04-14 23:08:04 +0100

    Refactor stations filtering code

--- airpollpy/src/stations.py
+++ airpollpy/src/stations.py
@@ -1,8 +1,8 @@
-from enum import Enum
 from pandas import DataFrame
 from src import csv_dataset
 
-FilterMode = Enum('FilterMode', 'max min')
+
+STATISTIC_VALUE = "statistic_value (µg/m3)"
 
 
 def get_stations_data(path: str) -> DataFrame:
@@ -10,33 +10,31 @@ def get_stations_data(path: str) -> DataFrame:
     return df
 
 
-def get_stations_filtered(df: DataFrame, func, filter_mode: str, pollutant: str) -> DataFrame:
+def get_stations_filtered(df: DataFrame, func) -> DataFrame:
     """
     :param df: stations data frame
     :param func: function to apply to the grouped by series statistic value
-    :param pollutant: the pollutant concerned
-    :param filter_mode: way to filter the stations
-    :return: the data set limited to the station that has been filtered per city
+    :return: the data set limited to the stations that has been filtered for each city
     """
 
-    df = df.loc[df.groupby(["city_name"])["statistic_value (µg/m3)"].apply(func)]
-    df.rename({'statistic_value (µg/m3)': filter_mode.name + ' ' + pollutant + ' (µg/m3)'}, axis=1, inplace=True)
-    return df
+    return df.loc[df.groupby(["city_name"])[STATISTIC_VALUE].apply(func)]
 
 
 def get_worst_stations(path: str, pollutant: str) -> DataFrame:
-    df = get_stations_data(path)
-    return get_stations_filtered(df, lambda x: x.idxmax(), FilterMode.max, pollutant)
+    df = get_stations_filtered(get_stations_data(path), lambda x: x.idxmax())
+    df.rename({STATISTIC_VALUE: 'max ' + pollutant + ' (µg/m3)'}, axis=1, inplace=True)
+    return df
 
 
 def get_best_stations(path: str, pollutant: str) -> DataFrame:
-    df = get_stations_data(path)
-    return get_stations_filtered(df, lambda x: x.idxmin(), FilterMode.min, pollutant)
+    df = get_stations_filtered(get_stations_data(path), lambda x: x.idxmin())
+    df.rename({STATISTIC_VALUE: 'min ' + pollutant + ' (µg/m3)'}, axis=1, inplace=True)
+    return df
 
 
 def get_mean_per_city(path: str, pollutant: str) -> DataFrame:
     df = get_stations_data(path)
-    df = df.groupby(["country iso code", "city_name"])["statistic_value (µg/m3)"].mean().to_frame()
-    df.rename({'statistic_value (µg/m3)': 'mean ' + pollutant + ' (µg/m3)'}, axis=1, inplace=True)
+    df = df.groupby(["country iso code", "city_name"])[STATISTIC_VALUE].mean().to_frame()
+    df.rename({STATISTIC_VALUE: 'mean ' + pollutant + ' (µg/m3)'}, axis=1, inplace=True)
     return df
 
