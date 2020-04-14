30fb4415bf03776fe858721d99e7274703ada85b
benoitantelme <benoitantelme@gmail.com>
2020-04-14 12:12:37 +0100

    Added data and tests for worst stations considering no2, o3 and pm25

--- airpollpy/tests/study/test_stations.py
+++ airpollpy/tests/study/test_stations.py
@@ -1,22 +1,59 @@
 from study.stations import get_stations_data
 from study.stations import get_worst_stations
 
-path = "../../data/main/cleaned/others/2013_pm10_monitoring_stations.csv"
+path1 = "../../data/main/cleaned/others/2013_"
+path2 = "_monitoring_stations.csv"
 
 
 def test_get_stations_data():
+    path = path1 + 'pm10' + path2
     df = get_stations_data(path)
     assert df is not None
 
 
-def test_get_worst_stations():
+def test_get_worst_pm10_stations():
+    path = path1 + 'pm10' + path2
     df = get_stations_data(path)
     size = len(df)
 
     df = get_worst_stations(path)
     assert df is not None
     assert len(df) < size
-    # number of ''traffic urban'' cities
+    # number of ''traffic urban'' cities for pm10
     assert len(df) == 317
 
 
+def test_get_worst_pm25_stations():
+    path = path1 + 'pm25' + path2
+    df = get_stations_data(path)
+    size = len(df)
+
+    df = get_worst_stations(path)
+    assert df is not None
+    assert len(df) < size
+    # number of ''traffic urban'' cities for pm25
+    assert len(df) == 135
+
+
+def test_get_worst_no2_stations():
+    path = path1 + 'no2' + path2
+    df = get_stations_data(path)
+    size = len(df)
+
+    df = get_worst_stations(path)
+    assert df is not None
+    assert len(df) < size
+    # number of ''traffic urban'' cities for no2
+    assert len(df) == 343
+
+
+def test_get_worst_o3_stations():
+    path = path1 + 'o3' + path2
+    df = get_stations_data(path)
+    size = len(df)
+
+    df = get_worst_stations(path)
+    assert df is not None
+    assert len(df) < size
+    # number of ''traffic urban'' cities for o3
+    assert len(df) == 98
