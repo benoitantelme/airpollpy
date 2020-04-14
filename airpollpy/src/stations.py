6f33be4c22f97e4ec7974d40ae88fe48ba84e473
benoitantelme <benoitantelme@gmail.com>
2020-04-14 17:50:17 +0100

    Replaced enum and test by apply and lambda

--- airpollpy/src/stations.py
+++ airpollpy/src/stations.py
@@ -2,7 +2,6 @@ from enum import Enum
 from pandas import DataFrame
 from src import csv_dataset
 
-
 FilterMode = Enum('FilterMode', 'max min')
 
 
@@ -11,33 +10,28 @@ def get_stations_data(path: str) -> DataFrame:
     return df
 
 
-def get_stations_filtered(df: DataFrame, filter_mode: FilterMode, pollutant: str) -> DataFrame:
+def get_stations_filtered(df: DataFrame, func, filter_mode: str, pollutant: str) -> DataFrame:
     """
+    :param df: stations data frame
+    :param func: function to apply to the grouped by series statistic value
     :param pollutant: the pollutant concerned
     :param filter_mode: way to filter the stations
-    :param df: stations data frame
-    :return: the data set limited to the worst station per city
+    :return: the data set limited to the station that has been filtered per city
     """
 
-    if filter_mode == FilterMode.max:
-        df = df.loc[df.groupby(["city_name"])["statistic_value (µg/m3)"].idxmax()]
-    elif filter_mode == FilterMode.min:
-        df = df.loc[df.groupby(["city_name"])["statistic_value (µg/m3)"].idxmin()]
-    else:
-        raise ValueError(filter_mode + ' is not an expected value')
-
+    df = df.loc[df.groupby(["city_name"])["statistic_value (µg/m3)"].apply(func)]
     df.rename({'statistic_value (µg/m3)': filter_mode.name + ' ' + pollutant + ' (µg/m3)'}, axis=1, inplace=True)
     return df
 
 
 def get_worst_stations(path: str, pollutant: str) -> DataFrame:
     df = get_stations_data(path)
-    return get_stations_filtered(df, FilterMode.max, pollutant)
+    return get_stations_filtered(df, lambda x: x.idxmax(), FilterMode.max, pollutant)
 
 
 def get_best_stations(path: str, pollutant: str) -> DataFrame:
     df = get_stations_data(path)
-    return get_stations_filtered(df, FilterMode.min, pollutant)
+    return get_stations_filtered(df, lambda x: x.idxmin(), FilterMode.min, pollutant)
 
 
 def get_mean_per_city(path: str, pollutant: str) -> DataFrame:
