3f4517a426e5af508ddfe5af30113e1518a6fbec
benoitantelme <benoitantelme@gmail.com>
2020-04-17 18:01:26 +0100

    Test for mean data set creation

--- airpollpy/tests/src/test_emissions.py
+++ airpollpy/tests/src/test_emissions.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 from data.constants import POLLUTANT
 from src import emissions
-from src.emissions import concat_two_sets, concat_sets, get_mean_frame
+from src.emissions import concat_two_sets, concat_sets, get_mean_frame, get_dataframe
 
 
 def test_get_dataframe():
@@ -26,14 +28,24 @@ def test_concat_sets():
     path = "../../data/main/cleaned/o3/Amsterdam/"
     concat = concat_sets(path, '2013')
     assert len(concat.columns) == 6
-    assert len(concat) == 68554
+    assert len(concat) == 34277
 
 
 def test_get_mean_frame():
     path = "../../data/main/cleaned/o3/Amsterdam/"
     df = concat_sets(path, '2013')
     df = get_mean_frame(df, POLLUTANT.o3)
-    print()
+    mean = df.iloc[0]['mean o3 (µg/m3)']
+
+    total = 0
+    nbr = 0
+    for path in Path(path).rglob('*2013*.csv'):
+        df2 = get_dataframe(path)
+        total += df2[df2['DatetimeBegin'] == '2013-01-01 00:00:00 +01:00']["Concentration"].item()
+        nbr += 1
+    assert mean == (total/nbr)
+
+
 
 
 
