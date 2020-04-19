d780ededdb1a3d6177b6badec3f8483d4e3a9e78
benoitantelme <benoitantelme@gmail.com>
2020-04-19 10:47:57 +0100

    First graph

--- airpollpy/study/emissions_study.py
+++ airpollpy/study/emissions_study.py
@@ -1,21 +1,12 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-from pandas import DataFrame, to_datetime
-
-from src.emissions import get_dataframe
-
-
-def mean_per_day(df: DataFrame) -> DataFrame:
-    print(df['DatetimeBegin'].dtypes)
-    df['DatetimeBegin'] = df['DatetimeBegin'].apply(lambda x: to_datetime(x))
-    df['Date'] = df['DatetimeBegin'].apply(lambda x: x.date())
-    df = df.groupby(["Countrycode", "AirPollutant", "UnitOfMeasurement", "Date"],
-                    as_index=False)["mean o3 (µg/m3)"].mean()
-    return df
-
+from src.emissions import get_dataframe, mean_per_day
 
 df = get_dataframe("../../data/main/cleaned/mean/Amsterdam_o3_2013.csv")
 mean_per_day(df)
-# ax = sns.lineplot(x="DatetimeBegin", y="mean o3 (µg/m3)", data=df)
-# plt.show()
+sns.set_style("whitegrid", {'grid.linestyle': '-'})
+plt.figure(figsize=(12, 6))
+ax = sns.lineplot(x="DatetimeBegin", y="mean o3 (µg/m3)", data=df).set_title('Amsterdam o3 emissions 2013')
+
+plt.show()
 
